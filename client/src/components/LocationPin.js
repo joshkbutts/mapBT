23d016e2c422236765a1d0c2883afb6b1a2a6fde
joshkbutts <joshuabutts37@gmail.com>
2021-03-02 17:18:27 -0500

    Fixed hover to work for all pics, added auth route

--- client/src/components/LocationPin.js
+++ client/src/components/LocationPin.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Icon from '@iconify/react'
 import locationIcon from '@iconify/icons-mdi/map-marker'
 import ReactTooltip from 'react-tooltip'
 
 const LocationPin = (props) => {
+  const [randomId, setRandomId] = useState(String(Math.random()))
   
   return (
     <div>
       <div className="pin">
-        <Icon icon={locationIcon} className="pin-icon" data-tip data-for="registerTip">
+        <Icon icon={locationIcon} className="pin-icon" data-tip data-for={randomId}>
         </Icon>
-        <ReactTooltip id="registerTip" place="top" effect="solid" className='tooltip'>
-          <img className='show-page-img' src={props.image}></img>
+        <ReactTooltip id={randomId} place="top" effect="solid" className='tooltip'>
+          <img className='show-page-img' src={props.image}></img> 
         </ReactTooltip>
       </div>
     </div>
